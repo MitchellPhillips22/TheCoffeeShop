965e974ce3154380fca9f204d79fa4f002458ece
Mitchell Phillips <phillips.mitchell22@yahoo.com>
2016-04-06 15:49:13 -0600

    Setting up storyboard for the first few view controllers(Home, Info, Menus)

---
+++ TheCoffeeShop/TheCoffeeShop/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  TheCoffeeShop
+//
+//  Created by Mitchell Phillips on 4/5/16.
+//  Copyright © 2016 Wasted Potential LLC. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TheCoffeeShop/TheCoffeeShop/NonCoffeeeMenuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NonCoffeeeMenuViewController.swift
+//  TheCoffeeShop
+//
+//  Created by Mitchell Phillips on 4/5/16.
+//  Copyright © 2016 Wasted Potential LLC. All rights reserved.
+//
+
+import UIKit
+
+class NonCoffeeeMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
