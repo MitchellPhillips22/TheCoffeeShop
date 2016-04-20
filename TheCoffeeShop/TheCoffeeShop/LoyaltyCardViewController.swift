33a9ee9a3b5580548a51fc983edcf49fa0b9f28c
Mitchell Phillips <phillips.mitchell22@yahoo.com>
2016-04-20 10:58:23 -0600

    Changed both events views to white background, fixed redeemable function on coffee tea side of loyalty

--- TheCoffeeShop/TheCoffeeShop/LoyaltyCardViewController.swift
+++ TheCoffeeShop/TheCoffeeShop/LoyaltyCardViewController.swift
@@ -77,6 +77,7 @@ class LoyaltyCardViewController: UIViewController {
         observeAuthCode()
         loadDefaults()
         checkForRedeemable()
+        checkForRedeemableCoffee()
         updateUI()
         
     }
